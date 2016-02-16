afbd69bbf970440601581ba6fd1833c5dfbbab3b
Kristian Flikka <kristian.flikka@gmail.com>
2016-02-16 16:04:07 +0100

    Some more pandas experiments

--- titanic-pandas/kaggle-tutorial.py
+++ titanic-pandas/kaggle-tutorial.py
@@ -1,15 +1,20 @@
 import pandas as pd
+import pylab as P
 
-
-def simple_stats():
-    data_frame = pd.read_csv('train.csv', header=0)
-    """:type:pandas.DataFrame"""
-    data_frame
+def read_dataframe(filename):
+    """@rtype: pandas.DataFrame"""
+    data_frame = pd.read_csv(filename, header=0)
+    return data_frame
 
 def main():
-    df = simple_stats()
-    df.
+    df = read_dataframe('train.csv')
+    #print(df[(df.Sex == 'male') & (df.Age > 60)].Cabin)
 
+    #df.Age.hist(bins=36)
+    #P.show()
+
+    df['Gender'] = df.Sex.map({'female':0, 'male':1}).astype(int)
+    
 
 if __name__ == "__main__":
     main()
