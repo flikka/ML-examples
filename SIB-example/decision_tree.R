9b7db6bf28fda246eef1bb6cc09eea429362df65
Kristian Flikka <kristian.flikka@gmail.com>
2016-05-06 13:11:08 +0200

    Changed bucket size to 1

--- SIB-example/decision_tree.R
+++ SIB-example/decision_tree.R
@@ -2,7 +2,7 @@ library(rpart)
 
 well_outcome <- read.table("well-example.csv", sep=";",row.name = "Well.ID", header=TRUE)
 
-fit <- rpart(Success ~ Feature.1 + Feature.2 + Feature.3, data=well_outcome, control=rpart.control(minbucket=0), method="class")
+fit <- rpart(Success ~ Feature.1 + Feature.2 + Feature.3, data=well_outcome, control=rpart.control(minbucket=1), method="class")
 
 plot(fit)
 text(fit, cex=.75)
